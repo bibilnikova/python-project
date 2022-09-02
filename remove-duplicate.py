49d61b35d82d822b4ef87f2d581654c8ddbefe3a
Katerina Bibilnikova <bibilnikova@gmail.com>
2022-09-02 19:54:34 +0300

    add new file

---
+++ remove-duplicate.py
@@ -0,0 +1,11 @@
+def removeDupicate(items):
+    list1 = []
+    for i in items:
+        if i not in list1:
+            list1.append(i)
+    return list1
+
+
+nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4, 5]
+print(removeDupicate(nums))
+
