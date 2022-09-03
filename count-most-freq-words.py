50ec070608896dde2531aa1e7bc790cb01b77e03
bibilnikova <74137044+bibilnikova@users.noreply.github.com>
2022-09-03 21:14:56 +0300

    Delete count-most-freq-words.py
    Signed-off-by: bibilnikova <74137044+bibilnikova@users.noreply.github.com>

--- count-most-freq-words.py
+++
@@ -1,9 +0,0 @@
-words = []
-with open('text.txt', 'r') as r:
-    for line in r:
-        words.extend(line.split())
-
-from collections import Counter
-counts = Counter(words)
-top5 = counts.most_common(5)
-print(top5)
