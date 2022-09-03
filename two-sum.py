a302e7c4dc3a79b0bae4217b92ce10baab2e9a76
bibilnikova <74137044+bibilnikova@users.noreply.github.com>
2022-09-03 21:14:26 +0300

    Delete two-sum.py
    Signed-off-by: bibilnikova <74137044+bibilnikova@users.noreply.github.com>

--- two-sum.py
+++
@@ -1,9 +0,0 @@
-def twosum(nums, target):
-    length = len(nums)
-    for i in range(length):
-        for j in range(i + 1, length):
-            if nums[i] + nums[j] == target:
-                return [i, j]
-
-
-print(twosum([2, 3, 5, 6], 5))
